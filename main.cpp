9650b9ce6ee92f435181f4162a91dd7f6cc1a14d
d-khatri <er.dheerajkhatri@gmail.com>
2014-06-06 04:06:20 +0530

    first commit in feature_X

--- main.cpp
+++ main.cpp
@@ -8,13 +8,8 @@ using namespace std;
 
 int main()
 {
-
-   cout<<"changed from git server\n";
-   int integer=0x303132;
-   char* p;
-   p=(char*)&integer;
-   if(*p=='0')cout<<"big endian \n"<<endl;
-   else if(*p=='2') cout<<"small endian\n";
-   else cout<<"error\n";
+	int i,j,k;
+	cout<<"this one is for another branch feature x\n";
+	cout<<"let's merge two branches and see what happens\n";
     return 0;
 }
